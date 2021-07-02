870abb85c08106f782eefe94624caf636d3109f8
정진범 <mz_gogosing@woowafriends.com>
2021-07-02 16:13:10 +0900

    Application init

---
+++ src/main/java/me/gogosing/SpringBootTestStudyApplication.java
@@ -0,0 +1,13 @@
+package me.gogosing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/gogosing/SpringBootTestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gogosing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
